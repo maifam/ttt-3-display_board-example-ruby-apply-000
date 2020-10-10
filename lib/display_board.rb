ded88a2ad5243a7b7307d31b754ac203efcecbb2
Mai Pham <maitphm@gmail.com>
2020-10-10 16:33:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board(line)
+  line = "   " + "|" + "   " + "|"
+end 
+
+puts display_board(line)
+puts -----------
+puts display_board(line)
+puts -----------
+puts display_board(line)
